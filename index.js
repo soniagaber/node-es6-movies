adec2cd30e5cd917204da5d50089de6e90e0ea3f
soniagaber <soniagaber90@gmail.com>
2023-11-08 14:48:34 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const movie = [
+    {
+    title: "You",
+    year: 2018,
+    genre: "Thriller psicologico",
+    rating: 8,
+    type: "tv",
+    seasons:3,
+    },
+    {
+    title: "Trainspotting",
+    year: 1996,
+    genre: "Drama",
+    rating: 8.8,
+    type: "movie",
+    },
+    {
+    title: "Breaking Bad",
+    year: 2008,
+    genre: "Drama",
+    rating: 9.5,
+    type: "tv",
+    seasons:5,
+    },
+    {
+    title: "Profumo - Storia di un assassino",
+    year: 2006,
+    genre: "Drama",
+    rating: 7,
+    type: "movie",
+    },
+];
